07a3df8a69a2044688d7b997f43c01d25f502084
Oscar Kemuel <oscar.kemuel5@gmail.com>
2022-05-20 21:21:44 -0300

    feat: implement functions

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const task: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, task])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+
+    const newTasks = [...tasks]
+    newTasks[taskIndex].isComplete = !newTasks[taskIndex].isComplete;
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task) => task.id !== id)
+
+    setTasks(newTasks)
   }
 
   return (
